b0c6c1239c4d635f16a9d141dc936f6d3d866c97
seansio1995 <chufan.xiao@west.cmu.edu>
2019-08-28 21:34:17 -0700

    first commit

---
+++ app/src/main/java/com/example/mssmsummerproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mssmsummerproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
